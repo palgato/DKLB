5bb4fa2f3452e22a2821ea1815b23816249e3662
palgato <davo.kelly@gmail.com>
2018-09-24 22:45:27 +0100

    Update 1: - Player Class - readFromFile method - writeToFile method

--- src/LeaderBoard.java
+++ src/LeaderBoard.java
@@ -1,32 +1,121 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class LeaderBoard
 {
+    String filePath;
+
+    public LeaderBoard(String leaderBoardFilePath) {
+        filePath = leaderBoardFilePath;
+    }
     public static void main(String[] args)
     {
-        String filepath = "DKLeaderBoard.csv";
-        createLeaderBoard(filepath);
+        //Create a new LeaderBoard with a specific file name
+        LeaderBoard unoBoard = new LeaderBoard("unoBoard.csv");
+
+        //Create a new HashMap that will store a set of Players
+        HashMap<Player, Integer> unoPlayers = new HashMap<Player, Integer>();
+
+        //Create new Players
+        // - need to prompt a user to specify these
+        Player dave = new Player("Dave",0);
+        Player donna = new Player("Donna",1);
+        Player bob = new Player("Bob",4);
+
+        //Add each Player to the HashMap
+        // - need to work out how to create a sequence for the keys
+        unoPlayers.put(dave,1);
+        unoPlayers.put(donna,2);
+        unoPlayers.put(bob,3);
+
+        //Create the LeaderBoard with the set of Players
+        unoBoard.createLeaderBoard(unoPlayers);
+
+        //Load an existing LeaderBoard
+        // - need to check if a CSV file exists and to load in all the Players in it
+        unoBoard.loadLeaderBoard();
     }
 
-    public static void createLeaderBoard(String fileName) {
+    /* Create a LeaderBoard file and add in a set of Players */
+    public void createLeaderBoard(HashMap<Player, Integer> players) {
+
+        //Cycle through each Player in the HashMap and write to the LeaderBoard file
+        for (Player p : players.keySet()) {
+                writeToFile(p.getName(),p.getWins());
+        }
+
+        //Print out successful creation of LeaderBoard file
+        System.out.println("CSV file " + filePath + " successfully created");
+    }
+
+    /* Load a LeaderBoard from a given file and display its contents */
+    public void loadLeaderBoard() {
+
+        //Create a HashMap of Players and their wins
+        HashMap<Player, Integer> showPlayers = new HashMap<Player, Integer>();
+
+        //Read from a given file and add Players and their wins to the HashMap
+        showPlayers = readFromFile(this.filePath);
+
+        //Display the Player and associated wins
+        for (Player p : showPlayers.keySet()) {
+            System.out.println("Player: " + p.getName() + "; Wins: " + p.getWins());
+        }
+    }
+
+    public HashMap<Player, Integer> readFromFile(String filePath){
+        String line;
+        int numLines = 0;
+        String csvSplitBy = ",";
+        HashMap<Player, Integer> loadedPlayers = new HashMap<Player, Integer>();
+
+        try {
+            FileReader fr = new FileReader(filePath);
+
+            BufferedReader br = new BufferedReader(fr);
+
+            while ((line = br.readLine()) != null) {
+                List<String> aList = new ArrayList<>(Arrays.asList(line.split(csvSplitBy)));
+                String n = aList.get(0);
+                int w = Integer.parseInt(aList.get(1));
+                Player p = new Player(n, w);
+                loadedPlayers.put(p,numLines);
+                numLines++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return loadedPlayers;
+    }
+
+    /* Add a Player - takes a name and creates a player in the LeaderBoard */
+    public void writeToFile(String writeName, String writeWins) {
+
+        //Create a writer which will write information to a file
         FileWriter writer = null;
 
         try {
-            writer = new FileWriter(fileName);
-            writer.append("Dave");
-            writer.append(',');
-            writer.append("1");
+            //Pass the LeaderBoard filePath to the FileWriter
+            writer = new FileWriter(this.filePath,true);
+
+            //Cycle through each Player in the HashMap as comma separated values
+            writer.append(writeName);
+            writer.append(",");
+            writer.append(writeWins);
             writer.append('\n');
 
-            System.out.println("CSV file created");
+            //Print out successful creation of LeaderBoard file
+            System.out.println("CSV file " + this.filePath + " successfully updated");
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
+
+                //Finish writing to the file and close it
                 writer.flush();
                 writer.close();
             } catch (IOException e) {
@@ -34,24 +123,6 @@ public class LeaderBoard
             }
         }
     }
-
-    /* Add a Player - takes a name and creates a player in the LeaderBoard */
-    public static void addPlayer(String playerName, String filepath) {
-
-/*        //setup mechanism to write to CSV file
-        //need to work out how to use this without using filepath as an argument to the method
-        FileWriter fw = new FileWriter(filepath,true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        PrintWriter pw = new PrintWriter(bw);
-
-        //append playerName to the file and allocate 0 wins by default
-        pw.println(playerName + "," + 0);
-        pw.flush();
-        pw.close();
-
-        //print out / display a successful message that the player has been added
-        System.out.println("Player " + playerName + " added!");*/
-}
     /* Remove a Player - takes a Player and removes them from the LeaderBoard */
     public void removePlayer(String player) {
         //
